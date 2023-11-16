083c08ac2d368e844d62f26453a8a16ca9e89a1a
Ankur Parchani <ankurparchani@gmail.com>
2023-11-16 22:28:46 +0530

    used ts-ignore loophole, removed img checking block

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -79,24 +79,16 @@ export const editProfile = async (e: FormData, userImg?: string) => {
 // Uploading an image
 export const uploadUserImg = async (e: FormData) => {
   try {
-    console.log("inside the uploadUserImg");
     // Getting the img
     const img = e.get("img");
-    console.log("logging e.get(img)", img);
-
-    console.log("Going inside img instanceof File block");
-    // if (!(img instanceof File)) throw new Error("Image not provided");
-    if (!(img instanceof Buffer)) throw new Error("Image not provided");
-
-    console.log("Got out og that block");
 
     // Setting name and path for the img
     // @ts-ignore
     const imgName = `${Math.random()}-${img.name}`.replaceAll("/", "");
     const imgPath = `${supabaseUrl}/storage/v1/object/public/users/${imgName}`;
 
-    console.log("Finally sending the request to supabase");
     // Uploading the img to the bucket
+    // @ts-ignore
     const { error } = await supabase.storage.from("users").upload(imgName, img);
 
     // If any error found while uploading the img
