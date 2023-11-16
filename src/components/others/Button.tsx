e6cdcd6fd903eaefee9d1cf4f18806cdff919316
Ankur Parchani <ankurparchani@gmail.com>
2023-11-16 22:53:33 +0530

    deployment changes: title and metadata

--- src/components/others/Button.tsx
+++ src/components/others/Button.tsx
@@ -17,9 +17,7 @@ function Button({ children, onClick, externalClass }: ButtonType) {
       onClick={onClick}
       className={`${
         externalClass ? externalClass : "bg-[#1d9bf0] hover:bg-[#51aeec] py-0.5"
-      } duration-200 mt-3  rounded-sm text-base ${
-        theme === "dark" ? "text-gray-100" : "text-gray-800"
-      }`}
+      } duration-200 mt-3  rounded-sm text-base`}
     >
       {children}
     </button>
